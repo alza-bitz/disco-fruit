e7c50b04c0c8f8c327efc486989ea8d32e30da1d
Alex Coyle <abcoyle@gmail.com>
2019-02-27 01:08:38 +0000

    Add outdated alias for depot
    Update deps
    Add org.clojure/tools.namespace dep to nrepl-server alias

--- test/disco_fruit/core_test.cljs
+++ test/disco_fruit/core_test.cljs
@@ -1,5 +1,7 @@
 (ns disco-fruit.core-test
   (:require
+   [clojure.test.check]
+   [clojure.test.check.properties]
    [disco-fruit.core :as c]
    [plumula.mimolette.alpha :refer [defspec-test]]))
 
